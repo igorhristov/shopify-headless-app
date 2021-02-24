4a4383b7251f1277723c140e5f990de9e5c88d58
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-02-24 22:38:59 +0100

    Building Navbar and Cart

--- frontend/src/context/shopContext.js
+++ frontend/src/context/shopContext.js
@@ -53,9 +53,13 @@ export class ShopProvider extends Component {
     this.setState({ product: product });
   };
 
-  closeCart = () => {};
+  closeCart = () => {
+    this.setState({ isCartOpen: false });
+  };
 
-  openCart = () => {};
+  openCart = () => {
+    this.setState({ isCartOpen: true });
+  };
 
   closeMenu = () => {};
 

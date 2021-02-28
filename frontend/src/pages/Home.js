594ab81580defe5cb605c744bb649e947f5a1084
Igor Hristov <58328456+igorhristov@users.noreply.github.com>
2021-03-01 00:38:02 +0100

    Image with Text component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,6 +4,7 @@ import { Box, Grid, Text, Image } from "@chakra-ui/react";
 
 import { ShopContext } from "../context/shopContext";
 import Hero from "../components/Hero";
+import ImageWithText from "../components/ImageWithText";
 
 const Home = () => {
   const { fetchAllProducts, products } = useContext(ShopContext);
@@ -27,6 +28,19 @@ const Home = () => {
           </Link>
         ))}
       </Grid>
+
+      <ImageWithText
+        image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/premium-bath-bombs.jpg?v=1610066758"
+        heading="Some Heading"
+        text="What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?"
+      />
+
+      <ImageWithText
+        reverse
+        image="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/bath-bomb-and-candle.jpg?v=1610066758"
+        heading="Another image Heading"
+        text="What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?"
+      />
     </Box>
   );
 };
